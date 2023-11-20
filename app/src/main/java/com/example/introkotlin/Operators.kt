08ab7e1ee1116d7142266c53624fe07ee0a2b0bb
AngelTravieso <angeltraviesoc@gmail.com>
2023-11-19 22:20:31 -0400

    Arithmetic Operators

---
+++ app/src/main/java/com/example/introkotlin/Operators.kt
@@ -0,0 +1,23 @@
+package com.example.introkotlin
+
+fun main() {
+
+    // Arithmetic Operators
+    var x: Float = 6f
+    var y: Float = 7f
+
+    println(x + y)
+    println(x - y)
+    println(x * y)
+    println(x / y)
+    println(x % y)
+
+    // Increment Operator
+    x++
+
+    // Decrement Operator
+    x--
+
+    println("X value: $x")
+
+}
