dd84c5607787ec30be3b1d1ea0275698a2f9b140
AngelTravieso <angeltraviesoc@gmail.com>
2023-11-19 22:13:51 -0400

    Booleans

---
+++ app/src/main/java/com/example/introkotlin/Boolean.kt
@@ -0,0 +1,13 @@
+package com.example.introkotlin
+
+fun main() {
+
+    // Boolean
+    val isKotlinFun : Boolean = false
+
+    // Boolean Expressions
+    val x = 10
+    val y = 5
+    print(x < y)
+
+}
