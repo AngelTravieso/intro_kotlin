078f47aef0538beb0063bb2595a755db2559d6e6
AngelTravieso <angeltraviesoc@gmail.com>
2023-11-19 22:09:04 -0400

    Numbers

---
+++ app/src/main/java/com/example/introkotlin/Numbers.kt
@@ -0,0 +1,25 @@
+package com.example.introkotlin
+
+fun main() {
+
+    // Byte: -128 to 127
+    val b: Byte = 50
+
+    // Short: -32786 to 32767
+    val s: Short = 32765
+
+    // Integers: -2147483648 to 2147483647
+    val i: Int = 99999
+
+    // Long
+    val l: Long = 7651654L
+
+    // Float
+    val f: Float = 63516981F
+
+    // Double
+    val d: Double = 651651.351651654
+
+    println(i)
+
+}
