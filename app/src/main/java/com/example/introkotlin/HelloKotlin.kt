cb639ab8cd66890a9b027ba7af4bd4cb282dff19
AngelTravieso <angeltraviesoc@gmail.com>
2023-11-19 21:53:49 -0400

    Variables

---
+++ app/src/main/java/com/example/introkotlin/HelloKotlin.kt
@@ -0,0 +1,26 @@
+package com.example.introkotlin
+
+fun main(){
+
+    // Variables
+    // var varialblename = value
+    // val variablename = value
+
+    var name : String = "Angel" // String (text)
+    val age : Int = 27       // Int (Number)
+
+    var city : String
+    city = "London"
+
+    val PI = 3.141592
+    name = "Jack"
+
+    var x = 6
+    var y = 10
+    println(x + y)
+
+    var name2 = "Smith"
+
+    print("$name $name2")
+
+}
